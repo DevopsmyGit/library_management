2d1d2f676f2425dd6ab2bd4f579caee75df35fbe
DevopsmyGit <136915876+DevopsmyGit@users.noreply.github.com>
2023-09-17 23:26:18 +0530

    Successfully created the Add Student

---
+++ lms-librarian/addStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lms_librarian
+{
+    public partial class addStudent : Form
+    {
+        public addStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addStudent_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
